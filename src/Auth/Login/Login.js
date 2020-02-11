6291b1518bb442f42a40dc3a2be2d2414edea50e
Rohit WW10 <rbandoo@ncsu.edu>
2020-02-11 12:36:58 -0500

    login form changes

--- src/Auth/Login/Login.js
+++ src/Auth/Login/Login.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import {Paper, Grid, Button, Checkbox, TextField, FormControlLabel, makeStyles, Box} from "@material-ui/core";
 import FlareComponent from "flare-react";
-
+import { Face, Fingerprint } from "@material-ui/icons";
+import "./Login.scss";
 const Login = () => {
+//   const classes = useStyles();
+const [animationName, setAnimationName] = useState("idle");
+const setUsernameFocus = () => {
+
+    animationName === 'idle' ? setAnimationName('test') : setAnimationName('idle')
+};
+const setTypingFocus = () => {
+    setAnimationName(animationName === 'idle' ? setAnimationName('test') : setAnimationName('idle'))
+}
   return (
-    <div>
-      <div>LOGIN</div>
-      <FlareComponent
-        width={200}
-        height={200}
-        animationName="check"
-        file="success-check.flr"
-      />
-    </div>
+  <div className="container">
+      <div className="center">
+      <div className="animationWrapper"><FlareComponent transparent={true} width={640} height={480} animationName={animationName} file="login-bear.flr"/></div>
+      <div className="formWrapper">
+      <TextField
+      onFocus={setUsernameFocus}
+      onfocusout={setTypingFocus}
+      
+    type="text"
+    label="Username"
+    variant="outlined"
+    color="secondary"
+    style={{width: '100%'}}
+  />
+   <Box pb={2.5} />
+        <TextField
+        type="password"
+    label="Password"
+    variant="outlined"
+    color="secondary"
+    style={{width: '100%'}}
+  />
+      </div>
+      </div>
+    
+  </div>
   );
 };
 
