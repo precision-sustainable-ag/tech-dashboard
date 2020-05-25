9b29816add331d98599988acf406a7a27921d6f4
Rohit Bandooni <rbandoo@ncsu.edu>
2020-05-25 06:06:08 -0400

    404

---
+++ src/PageNotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <Grid container>
+        <Typography variant="h4" align="center">
+          Unknown Route!
+        </Typography>
+      </Grid>
+    </div>
+  );
+};
+
+export default PageNotFound;
