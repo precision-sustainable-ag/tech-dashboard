d338f76f7606645780b5663b0cad000a70476c0e
Rohit Bandooni <rbandoo@ncsu.edu>
2020-06-25 03:03:44 -0400

    custom loader

--- src/utils/CustomComponents.js
+++ src/utils/CustomComponents.js
@@ -63,3 +63,85 @@ export const CustomLoader = (props) => {
   const color = props.color || "#3f51b5";
   return <Loading type="bars" width={width} height={height} color={color} />;
 };
+
+export const BarsLoader = (props) => {
+  const width = props.width || "200px";
+  const height = props.height || "200px";
+  const color = props.color || "#3f51b5";
+  return (
+    <div
+      style={{
+        fill: color,
+        height: height,
+        width: width,
+        margin: "0 auto",
+      }}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <path transform="translate(2)" d="M 0 12 V 20 H 4 V 12 Z">
+          <animate
+            attributeName="d"
+            values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z"
+            dur="1.2s"
+            repeatCount="indefinite"
+            begin="0"
+            keyTimes="0;.2;.5;1"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8"
+            calcMode="spline"
+          ></animate>
+        </path>
+        <path transform="translate(8)" d="M 0 12 V 20 H 4 V 12 Z">
+          <animate
+            attributeName="d"
+            values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z"
+            dur="1.2s"
+            repeatCount="indefinite"
+            begin="0.2"
+            keyTimes="0;.2;.5;1"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8"
+            calcMode="spline"
+          ></animate>
+        </path>
+        <path transform="translate(14)" d="M 0 12 V 20 H 4 V 12 Z">
+          <animate
+            attributeName="d"
+            values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z"
+            dur="1.2s"
+            repeatCount="indefinite"
+            begin="0.4"
+            keyTimes="0;.2;.5;1"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8"
+            calcMode="spline"
+          ></animate>
+        </path>
+        <path transform="translate(20)" d="M 0 11.098 V 20.902 H 4 V 11.098 Z">
+          <animate
+            attributeName="d"
+            values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z"
+            dur="1.2s"
+            repeatCount="indefinite"
+            begin="0.6"
+            keyTimes="0;.2;.5;1"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8"
+            calcMode="spline"
+          ></animate>
+        </path>
+        <path
+          transform="translate(26)"
+          d="M 0 6.98045 V 25.0195 H 4 V 6.98045 Z"
+        >
+          <animate
+            attributeName="d"
+            values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z"
+            dur="1.2s"
+            repeatCount="indefinite"
+            begin="0.8"
+            keyTimes="0;.2;.5;1"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8"
+            calcMode="spline"
+          ></animate>
+        </path>
+      </svg>
+    </div>
+  );
+};
