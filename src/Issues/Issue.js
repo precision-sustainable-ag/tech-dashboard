b2bb567ce3db6ef72ea0caca20526187d9d320f0
Rohit Bandooni <rbandoo@ncsu.edu>
2020-11-13 18:36:59 -0500

    issue components

---
+++ src/Issues/Issue.js
@@ -0,0 +1,155 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { ArrowBack } from "@material-ui/icons";
+import { Octokit } from "@octokit/rest";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../Store/Store";
+import { githubToken } from "../utils/api_secret";
+import { bannedRoles } from "../utils/constants";
+import MDEditor from "@uiw/react-md-editor";
+const showdown = require("showdown");
+
+const Issue = (props) => {
+  const issueNumber = props.match.params.issueNumber
+    ? parseInt(props.match.params.issueNumber)
+    : null;
+  const [state, dispatch] = useContext(Context);
+  const [showIssue, setShowIssue] = useState(false);
+  const [issueBody, setIssueBody] = useState([]);
+  const [newComment, setNewComment] = useState("");
+  const [newCommentAdded, setNewCommentAdded] = useState(false);
+
+  const octokit = new Octokit({ auth: githubToken });
+  const markdownConvert = new showdown.Converter();
+  useEffect(() => {
+    if (bannedRoles.includes(state.userRole)) {
+      setShowIssue(false);
+    } else {
+      fetchIssueComments();
+    }
+  }, [state.userRole]);
+
+  const addNewComment = async () => {
+    await octokit.request(
+      "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+      {
+        owner: "precision-sustainable-ag",
+        repo: "data_corrections",
+        issue_number: issueNumber,
+        body: newComment,
+      }
+    );
+  };
+
+  const fetchIssueComments = () => {
+    getIssueDetails().then((resp) => {
+      console.log(resp.data);
+      // console.log(markdownConvert.makeHtml(resp.data[0].body));
+      var issueBodyRev = [...resp.data].reverse();
+      setIssueBody(issueBodyRev);
+      setShowIssue(true);
+    });
+  };
+
+  const handleNewComment = () => {
+    addNewComment()
+      .then((resp) => {})
+      .then(() => {
+        setNewCommentAdded(!newCommentAdded);
+        setNewComment("");
+      });
+  };
+
+  useEffect(() => {
+    fetchIssueComments();
+  }, [newCommentAdded]);
+
+  useEffect(() => {
+    if (isNaN(issueNumber)) setShowIssue(false);
+    else setShowIssue(true);
+  }, [issueNumber]);
+
+  const getIssueDetails = async () => {
+    return await octokit.request(
+      "GET /repos/{owner}/{repo}/issues/{issue_number}/comments",
+      {
+        owner: "precision-sustainable-ag",
+        repo: "data_corrections",
+        issue_number: issueNumber,
+      }
+    );
+  };
+
+  return showIssue ? (
+    <Grid container spacing={5}>
+      <Grid item xs={12}>
+        <Grid container spacing={3}>
+          <Grid item>
+            <Button variant="contained" href="/issues" size="small">
+              {" "}
+              <ArrowBack /> All Issues
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <MDEditor value={newComment} onChange={setNewComment} />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleNewComment()}
+            >
+              Add Comment
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Grid container spacing={3}>
+          {issueBody.length > 0
+            ? issueBody.map((issueDetails, index) => (
+                <Grid item xs={12}>
+                  <Card style={{ width: "100%" }}>
+                    <CardContent>
+                      <Grid container>
+                        <Grid item xs={12}>
+                          <div
+                            dangerouslySetInnerHTML={{
+                              __html: markdownConvert
+                                .makeHtml(issueDetails.body)
+                                .toString(),
+                            }}
+                          ></div>
+                        </Grid>
+                      </Grid>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+            : ""}
+        </Grid>
+      </Grid>
+    </Grid>
+  ) : (
+    ""
+  );
+};
+
+export default Issue;
--- src/Issues/Issues.js
+++ src/Issues/Issues.js
@@ -1,35 +1,56 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Context } from "../Store/Store";
 import "./Issues.scss";
 import Axios from "axios";
 import Loading from "react-loading";
 import Skeleton from "@material-ui/lab/Skeleton";
 import RepositoriesComponent from "./Repositories";
-import { Box, Paper, Typography } from "@material-ui/core";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  Chip,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 import { bannedRoles } from "../utils/constants";
+import { createTokenAuth } from "@octokit/auth";
+import { Octokit } from "@octokit/rest";
+import { githubToken } from "../utils/api_secret";
+import { useAuth0 } from "../Auth/react-auth0-spa";
+import Issue from "./Issue";
+import { Done } from "@material-ui/icons";
 
-const getAllRepoNames = async url => {
+const getAllRepoNames = async (url) => {
   let data = await Axios.get(url)
-    .then(response => {
+    .then((response) => {
       //   console.log(response);
       return response.data;
     })
-    .then(response => {
+    .then((response) => {
       let dataObject = {};
       if (response.length > 0)
         dataObject = {
           success: true,
-          data: response
+          data: response,
         };
       else
         dataObject = {
           success: false,
-          data: response
+          data: response,
         };
 
       return dataObject;
     })
-    .then(bl => {
+    .then((bl) => {
       if (bl.success) {
         Promise.resolve("no error");
         return bl.data;
@@ -43,35 +64,133 @@ const getAllRepoNames = async url => {
 };
 
 export const ReposComponent = () => {
+  const octokit = new Octokit({ auth: githubToken });
   const [state, dispatch] = React.useContext(Context);
-  const [showIssues, setShowIssues] = React.useState(false);
+  const [showIssues, setShowIssues] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { user } = useAuth0();
+  const [issues, setIssues] = useState([]);
+  const [selectedState, setSelectedState] = useState("");
   const allReposAPIURL =
     "https://api.github.com/orgs/precision-sustainable-ag/repos";
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (bannedRoles.includes(state.userRole)) {
+      setLoading(false);
       setShowIssues(false);
     } else {
-      let fetchRepos = getAllRepoNames(allReposAPIURL);
-      fetchRepos.then(data => {
-        dispatch({
-          type: "UPDATE_ALL_REPOS",
-          data: data
+      // Github Oktokit
+
+      octokit
+        .request("GET /repos/{owner}/{repo}/collaborators", {
+          owner: "precision-sustainable-ag",
+          repo: "data_corrections",
+        })
+        .then((res) => {
+          const nicknames = res.data.map((response) => response.login);
+          if (nicknames.includes(user.nickname)) {
+            console.log("User exists");
+          } else {
+            setLoading(false);
+            console.log("User does not exist");
+          }
         });
-      });
+
       setShowIssues(true);
     }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [state.userRole]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return showIssues ? (
+  useEffect(() => {
+    // if (selectedState) {
+
+    getIssues(octokit, selectedState).then((resp) => {
+      setLoading(false);
+      setIssues(resp.data);
+      // setSelectedState(state.userInfo ? state.userInfo.state.)
+    });
+    // }
+  }, [selectedState]);
+
+  const [openDialog, setOpenDialog] = useState(false);
+  const [issueId, setIssueId] = useState(0);
+  return loading ? (
     <div className="issuesWrapper">
-      {state.repositories.length === 0 ? (
-        <div className="loaderRow">
-          <Loading type="cubes" color="#3f51b5" width="400px" height="400px" />
-        </div>
-      ) : (
-        <RepositoriesComponent data={state.repositories} />
-      )}
+      <div className="loaderRow">
+        <Loading type="cubes" color="#3f51b5" width="400px" height="400px" />
+      </div>
+    </div>
+  ) : showIssues ? (
+    <div className="issuesWrapper">
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <Typography variant="caption">
+            <b>Note:</b> This page is still under development
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {/* {selectedState */}
+          {/* ?  */}
+          <Grid container spacing={2}>
+            {state.userInfo.state.split(",").map((state, index) => (
+              <Grid item>
+                <Chip
+                  avatar={<Avatar>{state[0]}</Avatar>}
+                  clickable
+                  onClick={() => setSelectedState(state)}
+                  color={selectedState === state ? "primary" : "default"}
+                  label={state}
+                  key={index}
+                  size="medium"
+                  deleteIcon={selectedState === state ? <Done /> : ""}
+                />
+              </Grid>
+            ))}
+          </Grid>
+          {/* : ""} */}
+        </Grid>
+        <Grid item xs={12}>
+          {issues.length > 0 ? (
+            <>
+              {issues.map((issue, index) => (
+                <Grid container>
+                  <Grid item xs={12}>
+                    <Card style={{ width: "100%" }}>
+                      <CardActionArea href={`/issues/${issue.number}`}>
+                        <CardContent>
+                          <Typography variant="body1">{issue.title}</Typography>
+                          <Typography variant="body2">
+                            {"By: Rohit Bandooni"}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                </Grid>
+              ))}
+              <IssueDialog
+                open={openDialog}
+                issueId={issueId}
+                setOpen={setOpenDialog}
+              />
+            </>
+          ) : (
+            <Grid container spacing={5} alignItems="center" justify="center">
+              <Grid item xs={12}>
+                <Typography variant="h5" align="center">
+                  No issues for{" "}
+                  {state.userInfo.state.split(",").length > 1
+                    ? `"${state.userInfo.state.split(",").join(" and ")}"`
+                    : `"${state.userInfo.state}"`}
+                </Typography>
+              </Grid>
+
+              {/* <Button variant="contained" color="primary" size="large">
+                Create New Issue
+              </Button> */}
+            </Grid>
+          )}
+        </Grid>
+      </Grid>
     </div>
   ) : (
     <Box component={Paper} elevation={0}>
@@ -80,6 +199,36 @@ export const ReposComponent = () => {
       </Typography>
     </Box>
   );
+  // showIssues ? (
+  //   <div className="issuesWrapper">
+  //     <Issue />
+  //   </div>
+  // ) : (
+  // <Box component={Paper} elevation={0}>
+  //   <Typography variant={"h6"} align="center">
+  //     Your access level does not permit this action.
+  //   </Typography>
+  // </Box>
+  // );
 };
 
 export default ReposComponent;
+
+const getIssues = async (octokit, labels) => {
+  return await octokit.request("GET /repos/{owner}/{repo}/issues", {
+    owner: "precision-sustainable-ag",
+    repo: "data_corrections",
+    labels: labels,
+  });
+};
+
+const IssueDialog = ({ issueId, open = false, setOpen = () => {} }) => {
+  return (
+    <Dialog fullScreen open={open} onClose={setOpen(false)}>
+      <DialogTitle> Issue Title</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Issue Description</DialogContentText>
+      </DialogContent>
+    </Dialog>
+  );
+};
