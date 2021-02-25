8f8c5364cce5d4a6713612a432748994a58331fb
Rohit Bandooni <rbandoo@ncsu.edu>
2021-02-25 03:47:35 -0500

    drop IE 9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Dependency Imports
-import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
