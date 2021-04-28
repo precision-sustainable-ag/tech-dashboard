a4e280d0b7b4195f35f127b74e8c4c2bdbfb8f81
Rohit Bandooni <rbandooni@live.in>
2021-04-28 10:25:31 -0400

    node sass

--- src/Issues/components/IssueBodyBubble.js
+++ src/Issues/components/IssueBodyBubble.js
@@ -7,10 +7,51 @@ import Avatar from "@material-ui/core/Avatar";
 import MDEditor from "@uiw/react-md-editor";
 
 import "./IssueBodyBubble.scss";
-import { IconButton, Grid, Tooltip } from "@material-ui/core";
+import { IconButton, Grid, Tooltip, makeStyles } from "@material-ui/core";
 import { format_AM_PM } from "../../utils/constants";
+import styled from "styled-components";
+import clsx from "clsx";
+
+const ChatBubbleBody = styled.div`
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+  padding-left: 1.2em;
+  padding-right: 1.2em;
+  background-color: #f5f5f5;
+  border-radius: 20px;
+  font-size: 14px;
+  box-shadow: 0 2px 1px #2d2d2d;
+  &.dark {
+    color: black;
+  }
+  &.light {
+    color: black;
+  }
+
+  &.flex-end {
+    border-bottom-right-radius: 5px;
+    background-color: #2e7d32;
+    color: white;
+  }
+  &.flex-start {
+    border-top-left-radius: 5px;
+  }
+`;
+
+const useStyles = makeStyles({
+  chatBubbleText: {
+    "&:first-letter": {
+      textTransform: "capitalize",
+    },
+  },
+  chatBubbleAvatar: {
+    boxShadow: "0 0 6px 1px #2d2d2d",
+    border: 0,
+  },
+});
 
 const IssueBubbleBody = ({ issueData, user, isDarkTheme }) => {
+  const classes = useStyles();
   let side = issueData.hasMention
     ? issueData.user.login === user.nickname
       ? "flex-end"
@@ -46,7 +87,7 @@ const IssueBubbleBody = ({ issueData, user, isDarkTheme }) => {
                 rel="noreferrer"
               >
                 <Avatar
-                  className="chatBubbleAvatar"
+                  className={classes.chatBubbleAvatar}
                   src={
                     issueData.hasMention
                       ? `https://github.com/${
@@ -74,16 +115,15 @@ const IssueBubbleBody = ({ issueData, user, isDarkTheme }) => {
               justify={justify}
               alignItems={justify}
             >
-              <Grid item>
-                <div
-                  className={`chatBubbleBody ${justify} ${
+              {/* ${justify} ${
                     isDarkTheme ? `dark` : `light`
-                  }`}
-                >
+                  }` */}
+              <Grid item>
+                <ChatBubbleBody className={clsx(justify, {})}>
                   <Typography
                     variant="body1"
                     component="div"
-                    className="chatBubbleText"
+                    className={classes.chatBubbleText}
                     align={justify === "flex-start" ? "left" : "right"}
                   >
                     {issueData.viaEmail ? (
@@ -94,7 +134,7 @@ const IssueBubbleBody = ({ issueData, user, isDarkTheme }) => {
                       <MDEditor.Markdown source={issueData.body} />
                     )}
                   </Typography>
-                </div>
+                </ChatBubbleBody>
               </Grid>
               <Grid item xs={12}>
                 <Grid container justify={justify}>
