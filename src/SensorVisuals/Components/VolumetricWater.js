74e4ec9edebf4cce4197e98454d98125114e523f
Rohit Bandooni <rbandooni@live.in>
2021-07-16 07:29:08 -0400

    prettify

--- src/SensorVisuals/Components/VolumetricWater.js
+++ src/SensorVisuals/Components/VolumetricWater.js
@@ -9,7 +9,7 @@ import Highcharts from "highcharts";
 import { CustomLoader } from "../../utils/CustomComponents";
 
 const chartOptions = {
-  time:{
+  time: {
     timezoneOffset: new Date().getTimezoneOffset() * 2,
   },
   chart: {
@@ -64,12 +64,10 @@ const VolumetricWater = ({ tdrData }) => {
   // );
 
   useEffect(() => {
-    if(tdrData){
+    if (tdrData) {
       setData(tdrData);
       setLoading(false);
-    }
-    else
-      setLoading(true);
+    } else setLoading(true);
   }, [tdrData]);
 
   const bareSub1Data = useMemo(() => {
