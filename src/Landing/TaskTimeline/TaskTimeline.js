cd53dc06f250ac59f2c22d9142eadbd83139b23f
Rohit Bandooni <7522573+rbandooni@users.noreply.github.com>
2021-07-26 03:04:38 -0400

    html link changes Resolves #555

--- src/Landing/TaskTimeline/TaskTimeline.js
+++ src/Landing/TaskTimeline/TaskTimeline.js
@@ -1,31 +1,61 @@
 // Dependency Imports
-import React, { useMemo, useState } from "react";
-import { Typography, Grid, Paper } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Typography, Grid, Paper, Button } from "@material-ui/core";
 import { apiPassword, apiURL, apiUsername } from "../../utils/api_secret";
 import { Skeleton } from "@material-ui/lab";
+import parse, { attributesToProps } from "html-react-parser";
+import { Link } from "react-router-dom";
 
 // Default function
 const TaskTimeline = () => {
   const [htmlData, setHtmlData] = useState("");
 
-  let data = useMemo(() => {
+  // const ref = useRef();
+  useEffect(() => {
     const fetchData = async () => {
       let data = await fetch(`${apiURL}/api/psa/internal/pages/1592`, {
         headers: {
-          Authorization: "Basic " + btoa(`${apiUsername}:${apiPassword}`),
+          Authorization: `Basic ${Buffer.from(
+            apiUsername + ":" + apiPassword,
+            "binary"
+          ).toString("base64")}`,
         },
       });
-      return await data.text();
+      let response = await data.text();
+
+      setHtmlData(response);
     };
 
-    return fetchData();
+    fetchData();
   }, []);
 
-  data.then((resp) => {
-    resp = resp.replace('style="color: #000000;','');
-    setHtmlData(resp);
-  });
+  const options = {
+    replace: (domNode) => {
+      if (domNode.attribs && domNode.name === "a") {
+        const props = attributesToProps(domNode.attribs);
 
+        if (props.href.startsWith("https://onfarmtech.org/")) {
+          // domNode
+          console.log(domNode);
+          const relativeLink = props.href.replace("https://onfarmtech.org", "");
+          props.target = "_self";
+          return (
+            <Button
+              size="small"
+              component={Link}
+              to={relativeLink}
+              {...props}
+              variant="text"
+              style={{ textTransform: "capitalize" }}
+            >
+              {relativeLink.split("/").join("").split("-").join(" ")}
+            </Button>
+          );
+        }
+      }
+    },
+    trim: true,
+  };
   return (
     <Grid container>
       <Grid item xs={12}>
@@ -38,11 +68,9 @@ const TaskTimeline = () => {
         {!htmlData ? (
           <Skeleton width="100%" height="50vh" />
         ) : (
-          <Paper
-            style={{ padding: "1em" }}
-            elevation={3}
-            dangerouslySetInnerHTML={{ __html: htmlData ? htmlData : "" }}
-          />
+          <Paper style={{ padding: "1em" }} elevation={3}>
+            {parse(htmlData, options)}
+          </Paper>
         )}
       </Grid>
     </Grid>
