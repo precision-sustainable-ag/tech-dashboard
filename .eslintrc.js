e336f3e97f4e381f2d30fe77ae7fd5a96a0a9a1f
Rohit Bandooni <7522573+rbandooni@users.noreply.github.com>
2021-08-06 03:38:50 -0400

    new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -14,11 +18,23 @@ module.exports = {
   },
   plugins: ["react", "react-hooks"],
   rules: {
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
     "react-hooks/exhaustive-deps": "warn",
     "react-hooks/rules-of-hooks": "warn",
+    "react/no-deprecated": "warn",
+    "react/jsx-tag-spacing": "warn",
+    "react/jsx-curly-spacing": "warn",
+    // "react/no-multi-comp": "warn",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
     "react/jsx-pascal-case": "error",
+    "react/prop-types": "error",
+    // camelcase: "error",
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
   },
   settings: {
     react: {
