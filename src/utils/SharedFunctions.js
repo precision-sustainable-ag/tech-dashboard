033bc670e057de008c1b6c03a878339d2f6596b3
mikah <mspinega@ncsu.edu>
2021-08-20 14:29:38 -0400

    use new api

--- src/utils/SharedFunctions.js
+++ src/utils/SharedFunctions.js
@@ -146,7 +146,7 @@ const callAzureFunction = async (data, getTokenSilently) => {
 
   try {
     githubIssuesResponse = await fetch(
-      `https://githubissues.azurewebsites.us/api/GithubIssues`,
+      `https://premiumpythonapi.azurewebsites.us/api/GithubIssues`,
       options
     );
   } catch (err) {
