55101e2361b6b9504e8878f017e566aa6a4d68c9
Ubuntu <azureuser@TechDashboard.3hr5y0sbqsgetbs4mftcsi2qod.bx.internal.cloudapp.net>
2021-10-06 15:05:42 +0000

    resolve conflict

--- src/utils/SharedFunctions.js
+++ src/utils/SharedFunctions.js
@@ -146,11 +146,7 @@ export const callAzureFunction = async (data, endpoint, getTokenSilently) => {
 
   try {
     githubIssuesResponse = await fetch(
-<<<<<<< HEAD
       `https://premiumpythonapi.azurewebsites.net/api/${endpoint}`,
-=======
-      `https://premiumpythonapi.azurewebsites.net/api/GithubIssues`,
->>>>>>> master
       options
     );
   } catch (err) {
