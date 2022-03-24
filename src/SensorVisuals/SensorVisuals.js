6de4874175a0f50986e8fbc603efb1ae0e3e735b
pambawa <pambawa@ncsu.edu>
2022-03-24 18:32:17 -0400

    PR857: Comments resolved: Affiliations to ALL

--- src/SensorVisuals/SensorVisuals.js
+++ src/SensorVisuals/SensorVisuals.js
@@ -224,10 +224,10 @@ const SensorVisuals = (props) => {
 
           const uniqueAffiliations = Object.keys(groupBy(response, 'affiliation'))
             // .sort((a, b) => b - a)
-            .map((a,i) => {
+            .map((a) => {
               return {
                 affiliation: a,
-                active: i === 0,
+                active: false,
               };
             });
 
