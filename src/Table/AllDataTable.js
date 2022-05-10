452f059a888574f594bf38d7b0156042763abf25
Pritesh <psurana@ncsu.edu>
2022-05-10 11:15:34 -0700

    merge conflict

--- src/Table/AllDataTable.js
+++ src/Table/AllDataTable.js
@@ -141,6 +141,230 @@ const AllDataTable = (props) => {
       return false;
     }
   };
+  // const getAllTableData = async (url) => {
+  //   return await Axios({
+  //     url: url,
+  //     method: "get",
+  //     auth: {
+  //       username: apiUsername,
+  //       password: apiPassword,
+  //     },
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  // };
+
+  const [mapModalOpen, setMapModalOpen] = useState(false);
+  const [mapModalData, setMapModalData] = useState([]);
+
+  const ActionItems = ({ disabled = false, rowData }) => {
+    return (
+      <Grid container spacing={3}>
+        <Grid item>
+          {state.userInfo.view_type === 'home' && (
+            <Tooltip title="Edit field data">
+              <Button
+                size="small"
+                variant="contained"
+                startIcon={<Edit />}
+                color={state.isDarkTheme ? 'primary' : 'default'}
+                disabled={disabled}
+                onClick={() => {
+                  if (!disabled) {
+                    setEditModalOpen(true);
+                    setEditModalData(rowData);
+                  }
+                }}
+              >
+                Edit Data
+              </Button>
+            </Tooltip>
+          )}
+        </Grid>
+        <Grid item>
+          <Tooltip title="Reassign a new site">
+            <Button
+              size="small"
+              variant="contained"
+              startIcon={<Edit />}
+              color={state.isDarkTheme ? 'primary' : 'default'}
+              disabled={disabled}
+              onClick={() => {
+                if (!disabled) {
+                  setReassignSiteModalOpen(true);
+                  setReassignSiteModalData(rowData);
+                }
+              }}
+            >
+              Reassign Site
+            </Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <Tooltip title="Unenroll this site">
+            <Button
+              size="small"
+              variant="contained"
+              startIcon={<DeleteForever />}
+              color={state.isDarkTheme ? 'primary' : 'default'}
+              disabled={disabled}
+              onClick={() => {
+                if (!disabled) {
+                  setUnenrollOpen(true);
+                  setUnenrollRowData(rowData);
+                }
+              }}
+            >
+              Unenroll
+            </Button>
+          </Tooltip>
+        </Grid>
+      </Grid>
+    );
+  };
+  const RenderActionItems = ({ rowData }) => {
+    return UserIsEditor() ? (
+      <ActionItems rowData={rowData} disabled={false} />
+    ) : (
+      <ActionItems rowData={rowData} disabled={true} />
+    );
+  };
+  const CreateNewIssue = ({ rowData }) => {
+    return (
+      <Tooltip title="Submit a new issue">
+        <Button
+          startIcon={<QuestionAnswer />}
+          size="small"
+          variant="contained"
+          color={state.isDarkTheme ? 'primary' : 'default'}
+          onClick={() => {
+            setShowNewIssueDialog(true);
+            setNewIssueData(rowData);
+          }}
+        >
+          Comment
+        </Button>
+      </Tooltip>
+    );
+  };
+  const RenderLatLongMap = ({ rowData }) => {
+    const latLongNotPresent = rowData.latlng !== '' && rowData.latlng !== '-999' ? false : true;
+
+    return (
+      <Tooltip
+        title={latLongNotPresent ? 'Lat, long data not available' : 'View this field on a map'}
+      >
+        <span>
+          <Button
+            size="small"
+            disabled={latLongNotPresent}
+            startIcon={<Search />}
+            variant="contained"
+            color={state.isDarkTheme ? 'primary' : 'default'}
+            onClick={() => {
+              if (!latLongNotPresent) {
+                setMapModalData([parseFloat(rowData.latitude), parseFloat(rowData.longitude)]);
+                setMapModalOpen(true);
+              }
+            }}
+          >
+            {'Map'}
+          </Button>
+        </span>
+      </Tooltip>
+    );
+  };
+
+  const RenderActionsPanel = ({ rowData }) => {
+    // const filteredByValue = Object.fromEntries(
+    //   Object.entries(rowData).filter(
+    //     ([key, value]) => value === "" || value === "-999" || value === null
+    //   )
+    // );
+
+    // let blankEntities = Object.keys(filteredByValue).join(",").split(",");
+    // blankEntities =
+    //   blankEntities.length > 1
+    //     ? blankEntities.map((e, i) =>
+    //         i === blankEntities.length - 1 ? ` and ${e}` : `${e}, `
+    //       )
+    //     : blankEntities.toString();
+    return (
+      <Grid
+        container
+        spacing={3}
+        style={{
+          minHeight: '10vh',
+          paddingTop: '1em',
+          paddingBottom: '1em',
+          paddingLeft: '0.5em',
+          paddingRight: '0.5em',
+        }}
+      >
+        {/* <Grid item container>
+          {Object.keys(filteredByValue).length > 0 ? (
+            <Grid item xs={12}>
+              <Alert severity="info">
+                <Typography variant="body2" align="center">
+                  No record for <strong>{blankEntities}</strong>
+                </Typography>
+              </Alert>
+            </Grid>
+          ) : (
+            ""
+          )}
+        </Grid> */}
+        <Grid item xs={12}>
+          <InnerTable border="0">
+            <thead>
+              <tr>
+                <th colSpan="2" align="center">
+                  <Typography variant="body1">Contact Information</Typography>
+                </th>
+              </tr>
+            </thead>
+            <InnerTableBody>
+              <InnerTableRow>
+                <InnerTableCell>
+                  <Typography variant="body1">Phone Number</Typography>
+                </InnerTableCell>
+                <InnerTableCell>
+                  <Typography variant="body1">{rowData.phone}</Typography>
+                </InnerTableCell>
+              </InnerTableRow>
+              <InnerTableRow>
+                <InnerTableCell>
+                  <Typography variant="body1">Email</Typography>
+                </InnerTableCell>
+                <InnerTableCell>
+                  <Typography variant="body1">{rowData.email}</Typography>
+                </InnerTableCell>
+              </InnerTableRow>
+            </InnerTableBody>
+          </InnerTable>
+        </Grid>
+        <Grid
+          item
+          container
+          spacing={3}
+          // justifyContent="space-evenly"
+          // alignContent="center"
+          // alignItems="center"
+        >
+          <Grid item>
+            <RenderActionItems rowData={rowData} />
+          </Grid>
+          <Grid item>
+            <CreateNewIssue rowData={rowData} />
+          </Grid>
+          <Grid item>
+            <RenderLatLongMap rowData={rowData} />
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  };
 
   return showTable ? (
     loading ? (
