51040dc076e739d3de48a51bdda83ef6484965ab
Aaron Zeng <aaronz2003@gmail.com>
2022-06-22 22:02:55 -0400

    changed farm values table to material-table

--- src/Biomass/FarmValues.js
+++ src/Biomass/FarmValues.js
@@ -1,39 +1,22 @@
-import {
-  Grid,
-  Snackbar,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Button,
-} from '@material-ui/core';
+import { Grid, Snackbar } from '@material-ui/core';
 import React, { useState, useEffect, Fragment } from 'react';
 import { onfarmAPI } from '../utils/api_secret';
 import { BannedRoleMessage, CustomLoader } from '../utils/CustomComponents';
 import MuiAlert from '@material-ui/lab/Alert';
 import FarmValuesTable from './FarmValuesTable';
 import { useSelector } from 'react-redux';
-import styled from 'styled-components';
 
 // Helper function
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-const Header = styled.div`
-  font-size: 2rem;
-  margin-left: 1rem;
-`;
-
 const FarmValues = () => {
   const userInfo = useSelector((state) => state.userInfo);
   const [fetching, setFetching] = useState(true);
   const [farmValues, setFarmValues] = useState([]);
-  const [activeFarmYear, setActiveFarmYear] = useState(new Date().getFullYear());
-  const [activeAffiliation, setActiveAffliation] = useState('all');
   const [farmYears, setFarmYears] = useState([]);
   const [affiliations, setAffiliations] = useState([]);
-  const [units, setUnits] = useState('kg/ha');
   const [snackbarData, setSnackbarData] = useState({
     open: false,
     text: '',
@@ -64,15 +47,16 @@ const FarmValues = () => {
           let allYears = response.map((record) => record.year);
           setFarmYears([...new Set(allYears)]);
 
-          const affiliations = response
+          let allAffiliations = response
             .filter((record) => record.affiliation !== undefined)
             .reduce(
               (prev, curr) =>
                 !prev.includes(curr.affiliation) ? [...prev, curr.affiliation] : [...prev],
               [],
-            )
-            .sort();
-          setAffiliations(affiliations);
+            );
+          allAffiliations.sort();
+          allAffiliations.unshift('All');
+          setAffiliations(allAffiliations);
         })
         .then(() => {
           setFetching(false);
@@ -83,14 +67,6 @@ const FarmValues = () => {
         });
     }
   }, [userInfo.apikey, farmValues.length]);
-
-  const handleSelectChangeYear = (event) => {
-    setActiveFarmYear(event.target.value);
-  };
-
-  const handleSelectChangeAff = (event) => {
-    setActiveAffliation(event.target.value);
-  };
 
   return (
     <Grid container spacing={5} style={{ maxHeight: '90vh' }}>
@@ -113,69 +89,14 @@ const FarmValues = () => {
           >
             <Alert severity={snackbarData.severity}>{snackbarData.text}</Alert>
           </Snackbar>
-          {/* Years and Affiliation */}
-          <Grid container spacing={3} alignItems="center">
-            <Grid item>
-              <Header>Farm Values</Header>
-            </Grid>
-            <Grid item>
-              <FormControl>
-                <InputLabel id="year">Year</InputLabel>
-                <Select
-                  labelId="year"
-                  defaultValue={activeFarmYear}
-                  value={activeFarmYear}
-                  onChange={handleSelectChangeYear}
-                >
-                  {Object.keys(farmYears).map((year) => (
-                    <MenuItem key={farmYears[year]} value={farmYears[year]}>
-                      {farmYears[year]}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item>
-              <FormControl>
-                <InputLabel id="affiliations">Affiliations</InputLabel>
-                <Select
-                  labelId="affiliations"
-                  defaultValue={activeAffiliation}
-                  value={activeAffiliation}
-                  onChange={handleSelectChangeAff}
-                >
-                  <MenuItem key={'all'} value={'all'}>
-                    All
-                  </MenuItem>
-                  {Object.keys(affiliations).map((aff) => (
-                    <MenuItem key={affiliations[aff]} value={affiliations[aff]}>
-                      {affiliations[aff]}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item>
-              <Button
-                onClick={() => {
-                  if (units === 'kg/ha') setUnits('lbs/ac');
-                  else setUnits('kg/ha');
-                }}
-                color="primary"
-                variant="contained"
-              >
-                {units}
-              </Button>
-            </Grid>
-          </Grid>
           {/* Farm Values Table */}
           <Grid item container xs={12}>
             <FarmValuesTable
-              units={units}
               data={farmValues}
-              year={parseInt(activeFarmYear) || new Date().getFullYear()}
-              affiliation={activeAffiliation || 'all'}
+              snackbarData={snackbarData}
               setSnackbarData={setSnackbarData}
+              farmYears={farmYears}
+              affiliations={affiliations}
             />
           </Grid>
         </Fragment>
